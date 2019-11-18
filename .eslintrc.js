2ae8e64c9acee2006b628859d031e3fd997571b5
Gabriel Song <gabsong4@gmail.com>
2019-11-17 23:37:21 -0800

    Add ESLint configuration to repository

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-  "extends": "airbnb",
-  "installedESLint": true
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
 };
