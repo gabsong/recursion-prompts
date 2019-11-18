0b179e9d37e8a69362c3b600cb9d368c741ddb73
Gabriel Song <gabsong4@gmail.com>
2019-11-18 00:33:00 -0800

    Implement exponent() recursively, optimized for even exp

--- src/recursion.js
+++ src/recursion.js
@@ -86,6 +86,19 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  } else if (base < 0 && exp % 2 === -1) {
+    return -exponent(base, exp);
+  } else if (exp < 0) {
+    return 1 / (exponent(base, -exp));
+  } else if (exp % 2 === 1) {
+    return base * exponent(base, exp - 1);
+  } else if (exp % 2 === 0) {
+    // recursively compute y = x^(n/2) such that x^n = y*y
+    let y = exponent(base, exp / 2);
+    return y * y;
+  }
 };
 
 // 8. Determine if a number is a power of two.
